939f97e9e945c25cf9590d168655c31e49f60de1
Sopheak088 <ourlesson@gmail.com>
2019-10-23 13:05:35 +0700

    commit icon

--- Solution/School/Company/FrmCompanyList.Designer.cs
+++ Solution/School/Company/FrmCompanyList.Designer.cs
@@ -29,27 +29,27 @@
         private void InitializeComponent()
         {
             this.panelFilter = new System.Windows.Forms.Panel();
-            this.panelMain = new System.Windows.Forms.Panel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.rdoAllDay = new System.Windows.Forms.RadioButton();
-            this.rdoByDate = new System.Windows.Forms.RadioButton();
-            this.label1 = new System.Windows.Forms.Label();
-            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
-            this.dtpTo = new System.Windows.Forms.DateTimePicker();
-            this.label2 = new System.Windows.Forms.Label();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.rdoInactive = new System.Windows.Forms.RadioButton();
             this.rdoActive = new System.Windows.Forms.RadioButton();
-            this.txtKeyword = new System.Windows.Forms.TextBox();
-            this.btnSearch = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rdoByDate = new System.Windows.Forms.RadioButton();
+            this.rdoAllDay = new System.Windows.Forms.RadioButton();
+            this.panelMain = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panelFilter.SuspendLayout();
-            this.panelMain.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panelMain.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -61,15 +61,6 @@
             this.panelFilter.Name = "panelFilter";
             this.panelFilter.Size = new System.Drawing.Size(1024, 62);
             this.panelFilter.TabIndex = 0;
-            // 
-            // panelMain
-            // 
-            this.panelMain.Controls.Add(this.dataGridView1);
-            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelMain.Location = new System.Drawing.Point(0, 62);
-            this.panelMain.Name = "panelMain";
-            this.panelMain.Size = new System.Drawing.Size(1024, 201);
-            this.panelMain.TabIndex = 1;
             // 
             // groupBox1
             // 
@@ -84,18 +75,17 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Filter";
             // 
-            // panel1
+            // btnSearch
             // 
-            this.panel1.Controls.Add(this.dtpTo);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.dtpFrom);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.rdoByDate);
-            this.panel1.Controls.Add(this.rdoAllDay);
-            this.panel1.Location = new System.Drawing.Point(6, 16);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(451, 39);
-            this.panel1.TabIndex = 0;
+            this.btnSearch.Image = global::School.Properties.Resources.find_24x24;
+            this.btnSearch.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSearch.Location = new System.Drawing.Point(874, 23);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(86, 29);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "ស្វែងរក";
+            this.btnSearch.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSearch.UseVisualStyleBackColor = true;
             // 
             // panel2
             // 
@@ -108,63 +98,12 @@
             this.panel2.Size = new System.Drawing.Size(395, 39);
             this.panel2.TabIndex = 1;
             // 
-            // rdoAllDay
+            // txtKeyword
             // 
-            this.rdoAllDay.AutoSize = true;
-            this.rdoAllDay.Checked = true;
-            this.rdoAllDay.Location = new System.Drawing.Point(6, 7);
-            this.rdoAllDay.Name = "rdoAllDay";
-            this.rdoAllDay.Size = new System.Drawing.Size(60, 26);
-            this.rdoAllDay.TabIndex = 0;
-            this.rdoAllDay.TabStop = true;
-            this.rdoAllDay.Text = "គ្រប់ថ្ងៃ";
-            this.rdoAllDay.UseVisualStyleBackColor = true;
-            // 
-            // rdoByDate
-            // 
-            this.rdoByDate.AutoSize = true;
-            this.rdoByDate.Location = new System.Drawing.Point(72, 7);
-            this.rdoByDate.Name = "rdoByDate";
-            this.rdoByDate.Size = new System.Drawing.Size(60, 26);
-            this.rdoByDate.TabIndex = 1;
-            this.rdoByDate.Text = "តាមថ្ងៃ";
-            this.rdoByDate.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(138, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 22);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "ចាប់ពី";
-            // 
-            // dtpFrom
-            // 
-            this.dtpFrom.CustomFormat = "dd/MM/yyyy";
-            this.dtpFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpFrom.Location = new System.Drawing.Point(182, 6);
-            this.dtpFrom.Name = "dtpFrom";
-            this.dtpFrom.Size = new System.Drawing.Size(103, 30);
-            this.dtpFrom.TabIndex = 3;
-            // 
-            // dtpTo
-            // 
-            this.dtpTo.CustomFormat = "dd/MM/yyyy";
-            this.dtpTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpTo.Location = new System.Drawing.Point(327, 4);
-            this.dtpTo.Name = "dtpTo";
-            this.dtpTo.Size = new System.Drawing.Size(103, 30);
-            this.dtpTo.TabIndex = 5;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(291, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(30, 22);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "ដល់";
+            this.txtKeyword.Location = new System.Drawing.Point(250, 6);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(137, 30);
+            this.txtKeyword.TabIndex = 9;
             // 
             // label3
             // 
@@ -197,23 +136,85 @@
             this.rdoActive.Text = "មានសកម្មភាព";
             this.rdoActive.UseVisualStyleBackColor = true;
             // 
-            // txtKeyword
+            // panel1
             // 
-            this.txtKeyword.Location = new System.Drawing.Point(250, 6);
-            this.txtKeyword.Name = "txtKeyword";
-            this.txtKeyword.Size = new System.Drawing.Size(137, 30);
-            this.txtKeyword.TabIndex = 9;
+            this.panel1.Controls.Add(this.dtpTo);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.dtpFrom);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.rdoByDate);
+            this.panel1.Controls.Add(this.rdoAllDay);
+            this.panel1.Location = new System.Drawing.Point(6, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(451, 39);
+            this.panel1.TabIndex = 0;
             // 
-            // btnSearch
+            // dtpTo
             // 
-            this.btnSearch.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSearch.Location = new System.Drawing.Point(874, 23);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(86, 29);
-            this.btnSearch.TabIndex = 2;
-            this.btnSearch.Text = "ស្វែងរក";
-            this.btnSearch.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnSearch.UseVisualStyleBackColor = true;
+            this.dtpTo.CustomFormat = "dd/MM/yyyy";
+            this.dtpTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpTo.Location = new System.Drawing.Point(327, 4);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(103, 30);
+            this.dtpTo.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(291, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ដល់";
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.CustomFormat = "dd/MM/yyyy";
+            this.dtpFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpFrom.Location = new System.Drawing.Point(182, 6);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(103, 30);
+            this.dtpFrom.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ចាប់ពី";
+            // 
+            // rdoByDate
+            // 
+            this.rdoByDate.AutoSize = true;
+            this.rdoByDate.Location = new System.Drawing.Point(72, 7);
+            this.rdoByDate.Name = "rdoByDate";
+            this.rdoByDate.Size = new System.Drawing.Size(60, 26);
+            this.rdoByDate.TabIndex = 1;
+            this.rdoByDate.Text = "តាមថ្ងៃ";
+            this.rdoByDate.UseVisualStyleBackColor = true;
+            // 
+            // rdoAllDay
+            // 
+            this.rdoAllDay.AutoSize = true;
+            this.rdoAllDay.Checked = true;
+            this.rdoAllDay.Location = new System.Drawing.Point(6, 7);
+            this.rdoAllDay.Name = "rdoAllDay";
+            this.rdoAllDay.Size = new System.Drawing.Size(60, 26);
+            this.rdoAllDay.TabIndex = 0;
+            this.rdoAllDay.TabStop = true;
+            this.rdoAllDay.Text = "គ្រប់ថ្ងៃ";
+            this.rdoAllDay.UseVisualStyleBackColor = true;
+            // 
+            // panelMain
+            // 
+            this.panelMain.Controls.Add(this.dataGridView1);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(0, 62);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(1024, 201);
+            this.panelMain.TabIndex = 1;
             // 
             // dataGridView1
             // 
@@ -240,12 +241,12 @@
             this.Text = "CompanyList";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panelFilter.ResumeLayout(false);
-            this.panelMain.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panelMain.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
