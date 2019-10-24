291a2fd076ced57b5e016d904d1a847a80819649
Sopheak088 <vediolesson2907@gmail.com>
2019-10-24 09:26:13 +0700

    Rebuit

--- Solution/School/Program.cs
+++ Solution/School/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using School.Company;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace School
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmCompanyList());
         }
     }
 }
