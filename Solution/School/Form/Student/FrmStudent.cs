2da3826459de6f26cd8ae7efe21da7a0df2b69b7
Sopheak088 <vediolesson2907@gmail.com>
2019-10-24 10:04:49 +0700

    Studentform

---
+++ Solution/School/Form/Student/FrmStudent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace School.Form.Student
+{
+    public partial class FrmStudent : Form
+    {
+        public FrmStudent()
+        {
+            InitializeComponent();
+        }
+    }
+}
