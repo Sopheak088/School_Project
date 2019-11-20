0c25df524b5f2123ede1545cdce9182aa46e62cb
Sopheak088 <ourlesson031@gmail.com>
2019-11-20 12:58:14 +0700

    test

--- Solution/School/Teacher/FrmTeacher.Designer.cs
+++ Solution/School/Teacher/FrmTeacher.Designer.cs
@@ -151,10 +151,10 @@
             this.radioButton1.AutoSize = true;
             this.radioButton1.Location = new System.Drawing.Point(130, 48);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(94, 26);
+            this.radioButton1.Size = new System.Drawing.Size(52, 26);
             this.radioButton1.TabIndex = 27;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.Text = "ប្រុស";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // radioButton2
@@ -162,10 +162,10 @@
             this.radioButton2.AutoSize = true;
             this.radioButton2.Location = new System.Drawing.Point(243, 48);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(94, 26);
+            this.radioButton2.Size = new System.Drawing.Size(44, 26);
             this.radioButton2.TabIndex = 28;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.Text = "ស្រី";
             this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // FrmTeacher
